eaae1cdcc89d611400be052e585c28410cf1e6da
Brenden Hartman <brendenhart22@gmail.com>
2023-12-16 14:53:52 -0600

    YAY

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(arr){
+    return [arr[0],arr[1]];
+}
+
+function returnLastTwoDrivers(arr) {
+    const arrL = arr.length;
+    return [arr[arrL-2],arr[arrL-1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function (fare) {return fare * int;};
+}
+
+function fareDoubler(fare) {
+    const name = createFareMultiplier(2);
+    return name(fare);
+}
+
+function fareTripler(fare) {
+    const name = createFareMultiplier(3);
+    return name(fare);
+}
+
+function selectDifferentDrivers(arr, fun) {
+    return fun(arr);
+}
